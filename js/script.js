a6755e3eb140f113a9619e7f74205028aac43620
ShylaOommen <124379324+ShylaOommen@users.noreply.github.com>
2023-04-07 20:52:48 +0000

    updated everything

--- js/script.js
+++ js/script.js
@@ -12,12 +12,24 @@ function calculateSideC () {
   // input
   let sideA = parseFloat(document.getElementById('side-a').value);
   let sideB = parseFloat(document.getElementById('side-b').value);
-  let cosC = parseFloat(document.getElementById('angle-c').value) * Math.cos (1);
+  let angleC = parseFloat(document.getElementById('angle-c').value);
   
   // process
-  let side-c-sqrd = sideA ** 2 + sideB ** 2 - 2 * sideA * sideB * cosC;
-  let side-c-ans = Math.sqrt(side-c-sqrd);
+  let sideCAns = Math.sqrt(Math.pow (sideA, 2) + Math.pow (sideB, 2) * -2 * sideA * sideB * Math.cos (angleC))
 
   // output
-  document.getElementById('side-c-ans').innerHTML = "Side c is about" + side-c-ans.toFixed(2) + " cm." 
+  document.getElementById('side-c-ans').innerHTML = "Side c is about " + sideCAns.toFixed(2) + " cm." 
+}
+
+function calculateAngleC () {
+  // input
+  let sideAAng = parseFloat(document.getElementById('side-a-ang').value);
+  let sideBAng = parseFloat(document.getElementById('side-b-ang').value);
+  let sideC = parseFloat(document.getElementById('side-c').value);
+
+  //process
+  let AngleCAns = ((sideAAng ** 2 + sideBAng ** 2 - sideC ** 2)/2 * sideAAng * sideBAng)/Math.cos(1);
+
+  //output
+  document.getElementById('angle-c-ans').innerHTML = "Angle C is about " + AngleCAns.toFixed(2) + " °."
 }
