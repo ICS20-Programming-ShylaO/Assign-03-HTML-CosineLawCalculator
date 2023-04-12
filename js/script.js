5df59295962a43ae3ae7f457edc0322810342c98
ShylaOommen <124379324+ShylaOommen@users.noreply.github.com>
2023-04-12 15:39:31 +0000

    FINALLY FIXED script.js

--- js/script.js
+++ js/script.js
@@ -24,14 +24,28 @@ function calculateSideC () {
 
 function calculateAngleC () {
   // get sides a, b, and c
-  let sideA = parseFloat(document.getElementById('side-a-2').value);
-  let sideB = parseFloat(document.getElementById('side-b-2').value);
-  let sideC = parseFloat(document.getElementById('side-c').value);
+  let sideA_2 = parseFloat(document.getElementById('side-a-2').value);
+  let sideB_2 = parseFloat(document.getElementById('side-b-2').value);
+  let sideC_2 = parseFloat(document.getElementById('side-c').value);
 
+  // error checking
+  let angleCRad_2 = Math.acos( ( sideA_2 ** 2 + sideB_2 ** 2 - sideC_2 ** 2 ) / (2 * sideA_2 * sideB_2) );
+  let angleCAns_2 = angleCRad_2 * 180 / Math.PI;
+
+  // error checking
+  document.getElementById('angle-c-ans').innerHTML = angleCAns_2
+  
+  //let angleCAns = angleCRad * 180 / Math.PI;
+  
   //solve for angle C
-  let angleCRad = Math.acos((sideA ** 2 + sideB ** 2 - sideC ** 2)/(2 * sideA * sideB));
-  let angleCAns = (angleCRad * Math.PI / 180);
+  //let angleCRad = Math.acos((sideA ** 2 + sideB ** 2 - sideC ** 2)/(2 * sideA * sideB));
+
+  
+  
+  //let angleCAns = (angleCRad * Math.PI / 180);
+
+  
 
   //display to user
-  document.getElementById('angle-c-ans').innerHTML = "Angle C is about " + angleCAns.toFixed(2) + "°."
+  //document.getElementById('angle-c-ans').innerHTML = "Angle C is about " + angleCAns.toFixed(2) + "°."
 }
