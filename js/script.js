0c38177e7bbf3300c68d7588e1072b024042429b
ShylaOommen <124379324+ShylaOommen@users.noreply.github.com>
2023-04-13 00:54:15 +0000

    udpated evetrthing

--- js/script.js
+++ js/script.js
@@ -13,12 +13,13 @@ function calculateSideC () {
   let sideA = parseFloat(document.getElementById('side-a').value);
   let sideB = parseFloat(document.getElementById('side-b').value);
   let angleCDeg = parseFloat(document.getElementById('angle-c').value);
+  // convert degrees to radians
   let angleCRad = (angleCDeg * Math.PI / 180);
   
-  
+  // solve for side c
   let sideCAns = Math.sqrt(Math.pow(sideA, 2) + Math.pow(sideB, 2) - (2 * sideA * sideB * Math.cos(angleCRad)));
 
-  // display to user
+  // display side c to user
   document.getElementById('side-c-ans').innerHTML = "Side c is about " + sideCAns.toFixed(2) + " cm." 
 }
 
@@ -26,26 +27,12 @@ function calculateAngleC () {
   // get sides a, b, and c
   let sideA_2 = parseFloat(document.getElementById('side-a-2').value);
   let sideB_2 = parseFloat(document.getElementById('side-b-2').value);
-  let sideC_2 = parseFloat(document.getElementById('side-c').value);
+  let sideC_2 = parseFloat(document.getElementById('side-c-2').value);
 
-  // error checking
+  // solve for angle C
   let angleCRad_2 = Math.acos( ( sideA_2 ** 2 + sideB_2 ** 2 - sideC_2 ** 2 ) / (2 * sideA_2 * sideB_2) );
-  let angleCAns_2 = angleCRad_2 * 180 / Math.PI;
-
-  // error checking
-  document.getElementById('angle-c-ans').innerHTML = angleCAns_2
-  
-  //let angleCAns = angleCRad * 180 / Math.PI;
-  
-  //solve for angle C
-  //let angleCRad = Math.acos((sideA ** 2 + sideB ** 2 - sideC ** 2)/(2 * sideA * sideB));
-
-  
-  
-  //let angleCAns = (angleCRad * Math.PI / 180);
-
-  
-
-  //display to user
-  //document.getElementById('angle-c-ans').innerHTML = "Angle C is about " + angleCAns.toFixed(2) + "°."
+  // convert radians to degrees
+  let angleCAns_2 = (angleCRad_2 * 180 / Math.PI);
+  // display angle C to user
+  document.getElementById('angle-c-ans').innerHTML = "Angle C is " + angleCAns_2.toFixed(2) + "°."  
 }
